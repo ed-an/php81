f3d0dd6bd41783bd3b26c9deb23a2c82a1b213bd
Edmilson Neves <edmilsonaneves@gmail.com>
2023-07-13 15:06:20 -0300

    [Lesson1.1-ConstantsVariableVariables]: variable variable

--- src/public/index.php
+++ src/public/index.php
@@ -18,12 +18,26 @@ if (defined('LASTNAME')){
     echo LASTNAME;
 }
 
-
+// dinamic constants
 $paid = 'PAID';
-
 define('STATUS_'. $paid, $paid);
 
 echo STATUS_PAID;
 
 
+//variable variable
+// a variavel $foo recebe o valor de 'bar';
+$foo = 'bar';
+// Variavel de variavel é quando o valor de uma variavel recebe u
+//um valor, para isso atribua mais um dolar a variable $$foo
+// cujo valor passa a  ser variavel, assim $bar, cujo o valor é teste;
+//$$foo='teste'; é o mesmo que $bar = teste
+$$foo='teste';
+
+printf( "<br><br>'\$foo rebece ': %s<br>", $foo);
+printf( "<br> '\$\$foo' tem a variavel : $$foo");
+printf( "<br> '\$bar o valor é': %s<br>", $bar);
+
+echo "<br>$foo , {$$foo} <br>";
+echo "<br>$foo , ${foo} <br>";
 
