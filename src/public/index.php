37505732c6c35195d58c04e848fff689983a4892
Edmilson Neves <edmilsonaneves@gmail.com>
2023-07-13 14:13:17 -0300

    [Lesson1.1-ConstantsVariableVariables]: constants

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,29 @@
 <?php
 
-echo 'hello world';
+//define constants
+
+//function define
+define('FIRSTNAME', 'Edmilson');
+
+// key word const
+const LASTNAME=' Neves';
+
+//check if the constants exist
+if (defined('FIRSTNAME')){
+    echo FIRSTNAME;
+};
+
+
+if (defined('LASTNAME')){
+    echo LASTNAME;
+}
+
+
+$paid = 'PAID';
+
+define('STATUS_'. $paid, $paid);
+
+echo STATUS_PAID;
+
+
+
